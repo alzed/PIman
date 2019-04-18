76832601db0791f91aa53a8eb06ad66189c14747
Madhumitha P Ravishankar <madhumithap.ravishankar@gmail.com>
2019-04-18 22:38:03 +0530

    Added CLI

--- remove_background/main.py
+++ remove_background/main.py
@@ -1,16 +1,21 @@
 import os
-import glob
+import cv2
 import remove_bg
+import argparse
 
-input_data = #PATH to the input images folder
+ap = argparse.ArgumentParser()
+ap.add_argument("-src", "--source", required=True, help="Path to images source folder")
+ap.add_argument("-dst", "--destination", required=True, help="Path to images destination folder")
 
-masked_data = #Path to the manipulated images folder
+args = vars(ap.parse_args())
 
-for image_path in glob.glob(input_data)
+input_data = args["source"]
+output_data = args["destination"]
+
+for image in os.listdir(input_data):
+    image_path = os.path.join(input_data, image)
     img = remove_bg.remove_bg(image_path)
 
     #save to disk
-    img_name = image_path.split('/')[-1]
-    dest = os.path.join(masked_data, 'masked_' + img_name)
+    dest = os.path.join(output_data, 'masked_' + image)
     cv2.imwrite(dest, img)
-    
